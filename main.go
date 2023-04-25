a3b6b138c145c14194085e2ea69efed10f862886
Sherry El Garhy <sherry.elgarhy@reforge.com>
2023-04-25 16:41:33 +0200

    enhancements to UI and search features

--- main.go
+++ main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"bytes"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"index/suffixarray"
 	"io/ioutil"
 	"log"
 	"net/http"
+
 	"os"
+	"strings"
+
+	"index/suffixarray"
 )
 
+// Play represents a play and its quote.
+type Play struct {
+	Title        string
+	Player       string
+	ActSceneLine string
+	Quote        string
+}
+
+// Searcher is used to search for quotes in the Complete Works of Shakespeare.
+type Searcher struct {
+	CompleteWorks string
+	SuffixArray   *suffixarray.Index
+	Plays         []Play
+}
+
 func main() {
-	searcher := Searcher{}
-	err := searcher.Load("completeworks.txt")
+	// Load searchers from files
+	searcherCSV := Searcher{}
+	err := searcherCSV.Load("completeworkssorted.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	searcherTXT := Searcher{}
+	err = searcherTXT.Load("completeworks.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,7 +47,9 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	http.HandleFunc("/search", handleSearch(searcher))
+	// Setup HTTP server
+	http.HandleFunc("/search", handleSearch(searcherCSV))
+	http.HandleFunc("/search-context", handleSearchContext(searcherTXT))
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -35,24 +63,28 @@ func main() {
 	}
 }
 
-type Searcher struct {
-	CompleteWorks string
-	SuffixArray   *suffixarray.Index
-}
-
+// handleSearch is a function that takes a Searcher and returns an http.HandlerFunc.
+// It handles incoming HTTP requests and responds with search results.
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Parse the query parameter from the URL.
 		query, ok := r.URL.Query()["q"]
 		if !ok || len(query[0]) < 1 {
+			// If the query parameter is missing, return a bad request error.
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("missing search query in URL params"))
 			return
 		}
+		
+		// Perform the search using the provided Searcher.
 		results := searcher.Search(query[0])
+		
+		// Encode the search results as JSON and write the response.
 		buf := &bytes.Buffer{}
 		enc := json.NewEncoder(buf)
 		err := enc.Encode(results)
 		if err != nil {
+			// If there is an encoding error, return an internal server error.
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("encoding failure"))
 			return
@@ -62,21 +94,136 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 	}
 }
 
+// handleSearchContext returns a function that handles a search request.
+// It takes a Searcher as input, which is used to perform the search.
+// The returned function takes an http.ResponseWriter and http.Request,
+// extracts the search query from the URL parameters, performs the search,
+// and writes the results to the response in JSON format.
+func handleSearchContext(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Extract search query from URL parameters
+		query, ok := r.URL.Query()["q"]
+		if !ok || len(query[0]) < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("missing search query in URL params"))
+			return
+		}
+
+		// Perform search using the provided Searcher
+		results := searcher.Search(query[0])
+
+		// Encode search results in JSON format
+		buf := &bytes.Buffer{}
+		enc := json.NewEncoder(buf)
+		err := enc.Encode(results)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("encoding failure"))
+			return
+		}
+
+		// Write encoded search results to response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf.Bytes())
+	}
+}
+
+// Load reads data from a file with the specified filename and loads it into the Searcher.
+// If the file has a .csv extension, it is assumed to be a file containing play data,
+// which is parsed and stored in the Searcher's Plays field.
+// If the file has a .txt extension, it is assumed to be a file containing the complete works
+// of Shakespeare, which is loaded into the Searcher's CompleteWorks field and used to create
+// a SuffixArray for substring searches.
 func (s *Searcher) Load(filename string) error {
-	dat, err := ioutil.ReadFile(filename)
+	file, err := os.Open(filename)
 	if err != nil {
 		return fmt.Errorf("Load: %w", err)
 	}
-	s.CompleteWorks = string(dat)
-	s.SuffixArray = suffixarray.New(dat)
+	defer file.Close()
+
+	if strings.HasSuffix(filename, ".csv") {
+		// If file is a CSV, parse play data and store in Searcher's Plays field
+		reader := csv.NewReader(file)
+		reader.FieldsPerRecord = -1
+		records, err := reader.ReadAll()
+		if err != nil {
+			return fmt.Errorf("Load: %w", err)
+		}
+
+		for _, record := range records {
+			play := Play{
+				Title:        record[1],
+				ActSceneLine: record[2],
+				Player:       record[3],
+				Quote:        record[4],
+			}
+			s.Plays = append(s.Plays, play)
+		}
+	} else if strings.HasSuffix(filename, ".txt") {
+		// If file is a TXT, load complete works of Shakespeare and create SuffixArray for substring searches
+		dat, err := ioutil.ReadFile(filename)
+		if err != nil {
+			return fmt.Errorf("Load: %w", err)
+		}
+		s.CompleteWorks = string(dat)
+		s.SuffixArray = suffixarray.New(dat)
+	} else {
+		return fmt.Errorf("Load: unsupported file extension")
+	}
+
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
-	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+// Search searches for a given query string in the complete works and plays loaded into the Searcher.
+// It returns a slice of maps, where each map contains information about a single search result.
+// If the Searcher has a SuffixArray, it returns the 500 characters surrounding the first occurrence of the query string in the complete works as the "Context" field in the map.
+// If the Searcher has a Plays slice, it searches through each play's quotes and returns any quotes that contain the query string, along with the title, player, and act/scene/line information for that quote.
+// The function returns an empty slice if there are no matches.
+func (s *Searcher) Search(query string) []map[string]string {
+	// Create an empty slice of maps to hold the search results
+	results := []map[string]string{}
+
+	// If the Searcher has a SuffixArray, use it to perform a substring search on the CompleteWorks string
+	if s.SuffixArray != nil {
+		index := s.SuffixArray.Lookup([]byte(query), -1)
+
+		// If a match is found, create a map with a context field that contains the 500 characters surrounding the first match
+		if index != nil {
+			startIndex := index[0] - 125
+			if startIndex < 0 {
+				startIndex = 0
+			}
+			endIndex := index[0] + 125
+			if endIndex > len(s.CompleteWorks) {
+				endIndex = len(s.CompleteWorks)
+			}
+			result := map[string]string{
+				"Context": s.CompleteWorks[startIndex:endIndex],
+			}
+			results = append(results, result)
+		}
 	}
+
+	// If the Searcher has Plays, search for the query string in each play's Quote field
+	if s.Plays != nil {
+		for _, play := range s.Plays {
+			words := strings.Fields(play.Quote)
+			for _, word := range words {
+				if strings.EqualFold(word, query) {
+					// If a match is found, create a map with the play's Title, Player, Quote, and ActSceneLine fields
+					result := map[string]string{
+						"Title":        play.Title,
+						"Player":       play.Player,
+						"Quote":        play.Quote,
+						"ActSceneLine": play.ActSceneLine,
+					}
+					results = append(results, result)
+				}
+			}
+		}
+	}
+
+	// Return the slice of maps containing the search results, or an empty slice if no matches were found
 	return results
 }
--- static/app.js
+++ static/app.js
@@ -1,24 +1,129 @@
 const Controller = {
+  // The search method is triggered when the form is submitted.
+  // It prevents the default form submission, gets the search query from the form data,
+  // and sends a fetch request to the server to search for the query.
   search: (ev) => {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+
+    const response = fetch(`/search?q=${data.query}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      // When the response is received, it is converted to JSON format
+      // and passed to the updateCards method for rendering.
       response.json().then((results) => {
-        Controller.updateTable(results);
+        if (results.length === 0) {
+          const content = `
+            <p>Sorry, we couldn't find any results.</p>
+          `;
+          createAndOpenModal(content);
+        } else {
+          Controller.updateCards(results);
+        }
       });
     });
   },
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
+  // The updateCards method takes an array of search results and generates
+  // HTML code for each result to be displayed as a card on the webpage.
+  // It then updates the card container with the generated HTML.
+  updateCards: (results) => {
+    const container = document.getElementById("card-container");
+    const cards = [];
     for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+      const card = `
+        <div class="col s12 m6 l4">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">${result.Title}</span>
+              <p><strong>Character:</strong> ${result.Player}</p>
+              <p><strong>Quote:</strong> ${result.Quote}</p>
+              <p><strong>Act/Scene/Line:</strong> ${result.ActSceneLine}</p>
+            </div>
+            <div class="card-action">
+              <a class="more-button" data-quote="${result.Quote}">More</a>
+            </div>
+          </div>
+        </div>
+      `;
+      cards.push(card);
     }
-    table.innerHTML = rows;
+    container.innerHTML = cards.join("");
+
+    // Add event listeners to each "More" button so that when clicked,
+    // it triggers the showContext method to display additional text surrounding the quote.
+    const moreButtons = document.getElementsByClassName("more-button");
+    for (let button of moreButtons) {
+      button.addEventListener("click", Controller.showContext);
+    }
   },
+  
+  // This function is called when a user clicks on the "More" button in a search result card.
+  showContext: (ev) => {
+    // Get the quote from the "data-quote" attribute of the button that was clicked.
+    const quote = ev.target.dataset.quote;
+
+    // Send a request to the server to get the context for the quote.
+    const contextResponse = fetch(`/search-context?q=${quote}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+
+      // Once the server responds with the context, display it in a modal.
+      response.json().then((result) => {
+        if (result.length === 0) {
+          const content = `
+            <p>Sorry, we couldn't find any results.</p>
+          `;
+          createAndOpenModal(content);
+        } else {
+          // If context was found, format it with the search term highlighted and display it in a modal.
+          const context = result[0].Context;
+          const searchTerm = document.querySelector("#query").value;
+          const boldedSearchTerm = `<highlight>${searchTerm}</highlight>`;
+          const boldedContext = context.replaceAll(searchTerm, boldedSearchTerm);
+          const content = `
+            <p>...${boldedContext}...</p>
+          `;
+          createAndOpenModal(content);
+        }
+      });
+    });
+  },
+};  
+
+// This function creates a modal with the given content and opens it.
+const createAndOpenModal = (content) => {
+  const modal = `
+    <div class="modal">
+      <div class="modal-content">
+        ${content}
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+      </div>
+    </div>
+  `;
+
+  // Checks if a modal is already open and closes it if it is.
+  const existingModal = document.querySelector('.modal');
+  if (existingModal) {
+    existingModal.remove();
+  }
+
+  // Add the modal to the DOM and open it.
+  document.body.insertAdjacentHTML('beforeend', modal);
+  const modalElement = document.querySelector('.modal');
+  const modalInstance = M.Modal.init(modalElement);
+  modalInstance.open();
 };
 
+// Get the search form element and add a submit event listener to it.
 const form = document.getElementById("form");
 form.addEventListener("submit", Controller.search);
